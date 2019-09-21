9e03fe1db7b27cf2b44793afbb1c7fd7aad29857
ask4git <ask4git@gmail.com>
2019-09-21 18:48:46 +0900

    modified server.py

--- server.py
+++ server.py
@@ -3,5 +3,16 @@
 
 from app import app
 
-if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=8000, debug=True)
+# if __name__ == '__main__':
+#     app.run(host='127.0.0.1', port=8000, debug=True)
+
+from flask import Flask
+application = Flask(__name__)
+
+@application.route("/")
+def index():
+    return "Hello JinHyuk!"
+
+
+if __name__ == "__main__":
+    application.run(host='0.0.0.0', port='8000')
